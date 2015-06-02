a91ba4622f229d899c622802b79031ec2c6db148
Alex Cruz <mazdarider23@gmail.com>
2015-06-02 19:49:14 +0000

    Remove SystemUI restart for task manager
    Change-Id: I01984899b9491a8b6170e19820e067c8ccd4a140

--- src/com/android/settings/slim/StatusBar.java
+++ src/com/android/settings/slim/StatusBar.java
@@ -19,6 +19,7 @@ package com.android.settings.slim;
 import android.content.ContentResolver;
 import android.database.ContentObserver;
 import android.os.Bundle;
+<<<<<<< HEAD:src/com/android/settings/slim/StatusBar.java
 import android.os.Handler;
 import android.preference.ListPreference;
 import android.preference.Preference;
@@ -44,6 +45,35 @@ public class StatusBar extends SettingsPreferenceFragment implements OnPreferenc
     private static final String KEY_STATUS_BAR_TICKER = "status_bar_ticker";
     private static final String STATUS_BAR_BRIGHTNESS_CONTROL = "status_bar_brightness_control";
     private static final String KEY_STATUS_BAR_CLOCK = "clock_style_pref";
+=======
+import android.os.Handler;
+import android.preference.CheckBoxPreference;
+import android.preference.ListPreference;
+import android.preference.SwitchPreference;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.preference.PreferenceCategory;
+import android.preference.PreferenceScreen;
+import android.provider.Settings;
+import android.text.TextUtils;
+import android.text.Spannable;
+import android.text.SpannableStringBuilder;
+import android.provider.Settings.SettingNotFoundException;
+import android.util.Log;
+import android.widget.EditText;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.Utils;
+
+public class StatusBarSettings extends SettingsPreferenceFragment implements
+        OnPreferenceChangeListener {
+    private static final String TAG = "StatusBarSettings";
+
+    private static final String STATUS_BAR_BRIGHTNESS_CONTROL = "status_bar_brightness_control";
+    private static final String KEY_STATUS_BAR_CLOCK = "clock_style_pref";
+    private static final String KEY_STATUS_BAR_TICKER = "status_bar_ticker_enabled";
+>>>>>>> 47cd591... Remove SystemUI restart for task manager:src/com/android/settings/liquid/StatusBarSettings.java
     private static final String KEY_STATUS_BAR_NETWORK_ARROWS= "status_bar_show_network_activity";
     private static final String STATUS_BAR_SHOW_BATTERY_PERCENT = "status_bar_show_battery_percent";
     private static final String STATUS_BAR_BATTERY_STYLE = "status_bar_battery_style";
@@ -166,42 +196,37 @@ public class StatusBar extends SettingsPreferenceFragment implements OnPreferenc
     public void onResume() {
         super.onResume();
         updateClockStyleDescription();
-        updateStatusBarBrightnessControl();
         enableStatusBarBatteryDependents(String.valueOf(mbatteryStyle));
     }
 
-     @Override
-     public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen,
-             final Preference preference) {
-         final ContentResolver resolver = getActivity().getContentResolver();
+    @Override
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen,
+            final Preference preference) {
+        final ContentResolver resolver = getActivity().getContentResolver();
         if  (preference == mEnableTaskManager) {
             boolean checked = ((SwitchPreference)preference).isChecked();
             Settings.System.putInt(getActivity().getContentResolver(),
                     Settings.System.ENABLE_TASK_MANAGER, checked ? 1:0);
-            Helpers.restartSystemUI();
             return true;
         }
-         // If we didn't handle it, let preferences handle it.		
-         return super.onPreferenceTreeClick(preferenceScreen, preference);
-     }
-    private void updateStatusBarBrightnessControl() {
-        try {
-            if (mStatusBarBrightnessControl != null) {
-                int mode = Settings.System.getIntForUser(getContentResolver(),
-                    Settings.System.SCREEN_BRIGHTNESS_MODE,
-                    Settings.System.SCREEN_BRIGHTNESS_MODE_MANUAL);
+        // If we didn't handle it, let preferences handle it.		
+        return super.onPreferenceTreeClick(preferenceScreen, preference);
+    }
+    
+    private void updateCheckState(String value) {
+		if (value == null || TextUtils.isEmpty(value)) mStatusBarGreeting.setChecked(false);
+	}  
 
-                if (mode == Settings.System.SCREEN_BRIGHTNESS_MODE_AUTOMATIC) {
-                    mStatusBarBrightnessControl.setEnabled(false);
-                    mStatusBarBrightnessControl.setSummary(R.string.status_bar_toggle_info);
-                } else {
-                    mStatusBarBrightnessControl.setEnabled(true);
-                    mStatusBarBrightnessControl.setSummary(
-                        R.string.status_bar_toggle_brightness_summary);
-                }
-            }
-        } catch (SettingNotFoundException e) {
+    private void updateClockStyleDescription() {
+        if (mClockStyle == null) {
+            return;
         }
+        if (Settings.System.getInt(getContentResolver(),
+               Settings.System.STATUS_BAR_CLOCK, 1) == 1) {
+            mClockStyle.setSummary(getString(R.string.enabled));
+        } else {
+            mClockStyle.setSummary(getString(R.string.disabled));
+         }
     }
 
     private void updateNetworkArrowsSummary(int value) {
