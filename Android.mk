7f130b4f6c74b051bd6d835e074d5bca980f0b63
AlmightyMegadeath00 <>
2015-07-25 18:24:01 -0400

    Anti-Piracy Support [Settings][1/2]
    This is an example of how to bridge the service into settings or another
    package.

--- Android.mk
+++ Android.mk
@@ -14,6 +14,10 @@ LOCAL_SRC_FILES := \
         $(call all-java-files-under, src) \
         src/com/android/settings/EventLogTags.logtags
 
+LOCAL_SRC_FILES += \
+        $(call all-java-files-under, ../../services/AntiPiracy/src)
+
+
 LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res \
     frameworks/support/v7/cardview/res
 
@@ -32,7 +36,8 @@ LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
 LOCAL_AAPT_FLAGS := \
     --auto-add-overlay \
-    --extra-packages android.support.v7.cardview
+    --extra-packages android.support.v7.cardview \
+    --extra-packages org.antipiracy.support
 
 LOCAL_AAPT_INCLUDE_ALL_RESOURCES := true
 
