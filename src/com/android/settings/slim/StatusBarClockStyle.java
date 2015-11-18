42488f8df458d54867227fb5c764b1456c5503bf
Thecrazyskull <anaskarbila@gmail.com>
2015-11-18 21:28:30 +0100

    [NOT-PROPER] Fix build * Too lazy to actually add the icon :p Change-Id: Ifae84f21c265cb214f3419f11fee454f802096c0

--- src/com/android/settings/slim/StatusBarClockStyle.java
+++ src/com/android/settings/slim/StatusBarClockStyle.java
@@ -292,7 +292,6 @@ public class StatusBarClockStyle extends SettingsPreferenceFragment
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         menu.add(0, MENU_RESET, 0, R.string.reset)
-                .setIcon(R.drawable.ic_settings_reset)
                 .setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
     }
 
