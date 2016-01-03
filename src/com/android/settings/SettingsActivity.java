be50b5471880050f2b86044815252866a5c83599
InkSlinger666 <kengreenwood420666@gmail.com>
2016-01-02 22:09:59 -0600

    Hide gestures if not supported

--- src/com/android/settings/SettingsActivity.java
+++ src/com/android/settings/SettingsActivity.java
@@ -1252,7 +1252,8 @@ public class SettingsActivity extends Activity
                 boolean removeTile = false;
                 id = (int) tile.id;
                 if (id == R.id.operator_settings || id == R.id.manufacturer_settings
-                      || id == R.id.notification_slider) {
+                      || id == R.id.notification_slider
+                      || id == R.id.device_specific_gesture_settings) {
                     if (!Utils.updateTileToSpecificActivityFromMetaDataOrRemove(this, tile)) {
                         removeTile = true;
                     }
