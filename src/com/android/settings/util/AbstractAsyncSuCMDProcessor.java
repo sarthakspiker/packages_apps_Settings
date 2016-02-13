Tesla-M/packages_apps_Settings_backup

Add LCD Density Changer PS2: Add xxxhdpi PS3: Change LCD Density text to black PS4: Fix xxxhdpi values
Change-Id: I9893ce88a48a6fdbb68932c06fa34d3f3fa8862a

src/com/android/settings/tesla/AbstractAsyncSuCMDProcessor.java
@@ -1,0 +1,102 @@
+package com.android.settings.tesla;
+
+import android.os.AsyncTask;
+
+/**
+ * An abstract implentation of AsyncTask
+ *
+ * since our needs are simple send a command, perform a task when we finish
+ * this implentation requires you send the command as String...
+ * in the .execute(String) so you can send String[] of commands if needed
+ *
+ * This class is not for you if...
+ *     1) You do not need to perform any action after command execution
+ *        you want a Thread not this.
+ *     2) You need to perform more complex tasks in doInBackground
+ *        than simple script/command sequence of commands
+ *        you want your own AsyncTask not this.
+ *
+ * This class is for you if...
+ *     1) You need to run a command/script/sequence of commands without
+ *        blocking the UI thread and you must perform actions after the
+ *        task completes.
+ *     2) see #1.
+ */
+public abstract class AbstractAsyncSuCMDProcessor extends AsyncTask<String, Void, String> {
+    // if /system needs to be mounted before command
+    private boolean mMountSystem;
+    // return if we recieve a null command or empty command
+    public final String FAILURE = "failed_no_command";
+
+    /**
+     * Constructor that allows mounting/dismounting
+     * of /system partition while in background thread
+     */
+    public AbstractAsyncSuCMDProcessor(boolean mountSystem) {
+         this.mMountSystem = mountSystem;
+    }
+
+    /**
+     * Constructor that assumes /system should not be mounted
+     */
+    public AbstractAsyncSuCMDProcessor() {
+         this.mMountSystem = false;
+    }
+
+    /**
+     * DO NOT override this method you should simply send your commands off
+     * as params and expect to handle results in {@link #onPostExecute}
+     *
+     * if you find a need to @Override this method then you should
+     * consider using a new AsyncTask implentation instead
+     *
+     * @param params The parameters of the task.
+     *
+     * @return A result, defined by the subclass of this task.
+     */
+    @Override
+    protected String doInBackground(String... params) {
+        // don't bother if we don't get a command
+        if (params[0] == null || params[0].trim().equals(""))
+            return FAILURE;
+
+        String stdout = null;
+
+        // conditionally enforce mounting
+        if (mMountSystem) {
+            DensityHelpers.getMount("rw");
+        }
+        try {
+            // process all commands ***DO NOT SEND null OR ""; you have been warned***
+            for (int i = 0; params.length > i; i++) {
+                // always watch for null and empty strings, lazy devs :/
+                if (params[i] != null && !params[i].trim().equals("")) {
+                    stdout = CMDProcessor.runSuCommand(params[i]).getStdout();
+                } else {
+                    // bail because of careless devs
+                    return FAILURE;
+                }
+            }
+        // always unmount
+        } finally {
+            if (mMountSystem)
+                DensityHelpers.getMount("ro");
+        }
+        // return the stdout from the command
+        return stdout;
+    }
+
+    /**
+     * <p>Runs on the UI thread after {@link #doInBackground}. The
+     * specified result is the value returned by {@link #doInBackground}.</p>
+     *
+     * <p>This method won't be invoked if the task was cancelled.</p>
+     *
+     * You MUST @Override this method if you don't need the result
+     * then you should consider using a new Thread implentation instead
+     *
+     * @param result The result of the operation computed by {@link #doInBackground}.
+     */
+    @Override
+    protected abstract void onPostExecute(String result);
+}

src/com/android/settings/tesla/Density.java
@@ -1,0 +1,230 @@
+/*
+ * Copyright (C) 2013 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.tesla;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.IPackageDataObserver;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.PowerManager;
+import android.os.SystemProperties;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.preference.PreferenceScreen;
+import android.text.Editable;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.internal.logging.MetricsLogger;
+import com.android.settings.util.CMDProcessor;
+import com.android.settings.util.CommandResult;
+import com.android.settings.tesla.DensityHelpers;
+import com.android.settings.tesla.AbstractAsyncSuCMDProcessor;
+
+public class Density extends SettingsPreferenceFragment implements
+        OnPreferenceChangeListener {
+
+    private static final String TAG = "Density";
+
+    ListPreference mStockDensity;
+    Preference mReboot;
+    ListPreference mCustomDensity;
+
+    private static final int DIALOG_DENSITY = 101;
+    private static final int DIALOG_WARN_DENSITY = 102;
+
+    protected Context mContext;
+
+    int newDensityValue;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.density);
+
+        mContext = getActivity().getApplicationContext();
+
+        String currentDensity = SystemProperties.get("ro.sf.lcd_density");
+        PreferenceScreen prefs = getPreferenceScreen();
+
+        mStockDensity = (ListPreference) findPreference("stock_density");
+        mStockDensity.setOnPreferenceChangeListener(this);
+
+        mReboot = findPreference("reboot");
+
+        mCustomDensity = (ListPreference) findPreference("lcd_density");
+        mCustomDensity.setOnPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected int getMetricsCategory() {
+        return MetricsLogger.TESLACOIL;
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+        if (preference == mReboot) {
+            PowerManager pm = (PowerManager) getActivity()
+                    .getSystemService(Context.POWER_SERVICE);
+            pm.reboot("Resetting density");
+            return true;
+
+        }
+
+        return super.onPreferenceTreeClick(preferenceScreen, preference);
+    }
+
+    @Override
+    public Dialog onCreateDialog(int dialogId) {
+        LayoutInflater factory = LayoutInflater.from(mContext);
+
+        switch (dialogId) {
+            case DIALOG_DENSITY:
+                final View textEntryView = factory.inflate(
+                        R.layout.alert_dialog_text_entry, null);
+                return new AlertDialog.Builder(getActivity())
+                        .setTitle(getResources().getString(R.string.set_custom_density_title))
+                        .setView(textEntryView)
+                        .setPositiveButton(getResources().getString(R.string.set_custom_density_set), new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int whichButton) {
+                                EditText dpi = (EditText) textEntryView.findViewById(R.id.dpi_edit);
+                                Editable text = dpi.getText();
+                                Log.i(TAG, text.toString());
+
+                                try {
+                                    newDensityValue = Integer.parseInt(text.toString());
+                                    showDialog(DIALOG_WARN_DENSITY);
+                                } catch (Exception e) {
+                                    mCustomDensity.setSummary(getResources().getString(R.string.custom_density_summary_invalid));
+                                }
+
+                            }
+                        })
+                        .setNegativeButton(getResources().getString(R.string.cancel), new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int whichButton) {
+
+                                dialog.dismiss();
+                            }
+                        }).create();
+            case DIALOG_WARN_DENSITY:
+                return new AlertDialog.Builder(getActivity())
+                        .setTitle(getResources().getString(R.string.custom_density_dialog_title))
+                        .setMessage(
+                                getResources().getString(R.string.custom_density_dialog_summary))
+                        .setCancelable(false)
+                        .setNeutralButton(getResources().getString(R.string.custom_density_dialog_button_got), new DialogInterface.OnClickListener() {
+
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                                if (setLcdDensity(newDensityValue)) {
+                                    mCustomDensity.setSummary(newDensityValue + "");
+                                }
+                            }
+                        })
+                        .setPositiveButton(getResources().getString(R.string.custom_density_dialog_button_reboot), new DialogInterface.OnClickListener() {
+
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                if (setLcdDensity(newDensityValue)) {
+                                    PowerManager pm = (PowerManager) getActivity()
+                                            .getSystemService(Context.POWER_SERVICE);
+                                    pm.reboot("Resetting density");
+                                }
+                            }
+                        })
+                        .setNegativeButton(getResources().getString(R.string.cancel),
+                                new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        dialog.dismiss();
+                                    }
+                                })
+                        .create();
+        }
+        return null;
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mCustomDensity) {
+            String strValue = (String) newValue;
+            if (strValue.equals(getResources().getString(R.string.custom_density))) {
+                showDialog(DIALOG_DENSITY);
+                return true;
+            } else {
+                newDensityValue = Integer.parseInt((String) newValue);
+                showDialog(DIALOG_WARN_DENSITY);
+                return true;
+            }
+        } else if (preference == mStockDensity) {
+            newDensityValue = Integer.parseInt((String) newValue);
+            if (setLcdDensity(newDensityValue)) {
+                mStockDensity.setSummary(getResources().getString(
+                    R.string.stock_density_changed_summary) + newDensityValue);
+            }
+            return true;
+        }
+
+        return false;
+    }
+
+    private boolean setLcdDensity(int newDensity) {
+        DensityHelpers.getMount("rw");
+        if (!CMDProcessor.runSuCommand(
+        "busybox sed -i 's|ro.sf.lcd_density=.*|" + "ro.sf.lcd_density" + "=" + newDensity + "|' " + "/system/build.prop")
+        .success())
+        {
+            showRootDeniedInfoDialog();
+            return false;
+        }
+        DensityHelpers.getMount("ro");
+        return true;
+    }
+
+    private void showRootDeniedInfoDialog() {
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(getActivity());
+        alertDialog.setTitle(R.string.su_dialog_error_title);
+        alertDialog.setMessage(R.string.su_dialog_error_message);
+        alertDialog.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+            }
+        });
+        alertDialog.create().show();
+    }
+}

sarthakspiker/packages_apps_Settings

Signed-off-by: Martinusbe <martinusbe@gmail.com>
Conflicts:
	src/com/android/settings/DisplaySettings.java

src/com/android/settings/util/AbstractAsyncSuCMDProcessor.java
@@ -1,0 +1,102 @@
+package com.android.settings.util;
+
+import android.os.AsyncTask;
+
+/**
+ * An abstract implentation of AsyncTask
+ *
+ * since our needs are simple send a command, perform a task when we finish
+ * this implentation requires you send the command as String...
+ * in the .execute(String) so you can send String[] of commands if needed
+ *
+ * This class is not for you if...
+ *     1) You do not need to perform any action after command execution
+ *        you want a Thread not this.
+ *     2) You need to perform more complex tasks in doInBackground
+ *        than simple script/command sequence of commands
+ *        you want your own AsyncTask not this.
+ *
+ * This class is for you if...
+ *     1) You need to run a command/script/sequence of commands without
+ *        blocking the UI thread and you must perform actions after the
+ *        task completes.
+ *     2) see #1.
+ */
+public abstract class AbstractAsyncSuCMDProcessor extends AsyncTask<String, Void, String> {
+    // if /system needs to be mounted before command
+    private boolean mMountSystem;
+    // return if we recieve a null command or empty command
+    public final String FAILURE = "failed_no_command";
+
+    /**
+     * Constructor that allows mounting/dismounting
+     * of /system partition while in background thread
+     */
+    public AbstractAsyncSuCMDProcessor(boolean mountSystem) {
+         this.mMountSystem = mountSystem;
+    }
+
+    /**
+     * Constructor that assumes /system should not be mounted
+     */
+    public AbstractAsyncSuCMDProcessor() {
+         this.mMountSystem = false;
+    }
+
+    /**
+     * DO NOT override this method you should simply send your commands off
+     * as params and expect to handle results in {@link #onPostExecute}
+     *
+     * if you find a need to @Override this method then you should
+     * consider using a new AsyncTask implentation instead
+     *
+     * @param params The parameters of the task.
+     *
+     * @return A result, defined by the subclass of this task.
+     */
+    @Override
+    protected String doInBackground(String... params) {
+        // don't bother if we don't get a command
+        if (params[0] == null || params[0].trim().equals(""))
+            return FAILURE;
+
+        String stdout = null;
+
+        // conditionally enforce mounting
+        if (mMountSystem) {
+            DensityHelpers.getMount("rw");
+        }
+        try {
+            // process all commands ***DO NOT SEND null OR ""; you have been warned***
+            for (int i = 0; params.length > i; i++) {
+                // always watch for null and empty strings, lazy devs :/
+                if (params[i] != null && !params[i].trim().equals("")) {
+                    stdout = CMDProcessor.runSuCommand(params[i]).getStdout();
+                } else {
+                    // bail because of careless devs
+                    return FAILURE;
+                }
+            }
+        // always unmount
+        } finally {
+            if (mMountSystem)
+                DensityHelpers.getMount("ro");
+        }
+        // return the stdout from the command
+        return stdout;
+    }
+
+    /**
+     * <p>Runs on the UI thread after {@link #doInBackground}. The
+     * specified result is the value returned by {@link #doInBackground}.</p>
+     *
+     * <p>This method won't be invoked if the task was cancelled.</p>
+     *
+     * You MUST @Override this method if you don't need the result
+     * then you should consider using a new Thread implentation instead
+     *
+     * @param result The result of the operation computed by {@link #doInBackground}.
+     */
+    @Override
+    protected abstract void onPostExecute(String result);
+}

src/com/android/settings/util/Density.java
@@ -1,0 +1,230 @@
+/*
+ * Copyright (C) 2013 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.util;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.IPackageDataObserver;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.PowerManager;
+import android.os.SystemProperties;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.preference.PreferenceScreen;
+import android.text.Editable;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.internal.logging.MetricsLogger;
+import com.android.settings.util.CMDProcessor;
+import com.android.settings.util.CommandResult;
+import com.android.settings.util.DensityHelpers;
+import com.android.settings.util.AbstractAsyncSuCMDProcessor;
+
+public class Density extends SettingsPreferenceFragment implements
+        OnPreferenceChangeListener {
+
+    private static final String TAG = "Density";
+
+    ListPreference mStockDensity;
+    Preference mReboot;
+    ListPreference mCustomDensity;
+
+    private static final int DIALOG_DENSITY = 101;
+    private static final int DIALOG_WARN_DENSITY = 102;
+
+    protected Context mContext;
+
+    int newDensityValue;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.density);
+
+        mContext = getActivity().getApplicationContext();
+
+        String currentDensity = SystemProperties.get("ro.sf.lcd_density");
+        PreferenceScreen prefs = getPreferenceScreen();
+
+        mStockDensity = (ListPreference) findPreference("stock_density");
+        mStockDensity.setOnPreferenceChangeListener(this);
+
+        mReboot = findPreference("reboot");
+
+        mCustomDensity = (ListPreference) findPreference("lcd_density");
+        mCustomDensity.setOnPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected int getMetricsCategory() {
+        return MetricsLogger.MAIN_SETTINGS;
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+        if (preference == mReboot) {
+            PowerManager pm = (PowerManager) getActivity()
+                    .getSystemService(Context.POWER_SERVICE);
+            pm.reboot("Resetting density");
+            return true;
+
+        }
+
+        return super.onPreferenceTreeClick(preferenceScreen, preference);
+    }
+
+    @Override
+    public Dialog onCreateDialog(int dialogId) {
+        LayoutInflater factory = LayoutInflater.from(mContext);
+
+        switch (dialogId) {
+            case DIALOG_DENSITY:
+                final View textEntryView = factory.inflate(
+                        R.layout.alert_dialog_text_entry, null);
+                return new AlertDialog.Builder(getActivity())
+                        .setTitle(getResources().getString(R.string.set_custom_density_title))
+                        .setView(textEntryView)
+                        .setPositiveButton(getResources().getString(R.string.set_custom_density_set), new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int whichButton) {
+                                EditText dpi = (EditText) textEntryView.findViewById(R.id.dpi_edit);
+                                Editable text = dpi.getText();
+                                Log.i(TAG, text.toString());
+
+                                try {
+                                    newDensityValue = Integer.parseInt(text.toString());
+                                    showDialog(DIALOG_WARN_DENSITY);
+                                } catch (Exception e) {
+                                    mCustomDensity.setSummary(getResources().getString(R.string.custom_density_summary_invalid));
+                                }
+
+                            }
+                        })
+                        .setNegativeButton(getResources().getString(R.string.cancel), new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int whichButton) {
+
+                                dialog.dismiss();
+                            }
+                        }).create();
+            case DIALOG_WARN_DENSITY:
+                return new AlertDialog.Builder(getActivity())
+                        .setTitle(getResources().getString(R.string.custom_density_dialog_title))
+                        .setMessage(
+                                getResources().getString(R.string.custom_density_dialog_summary))
+                        .setCancelable(false)
+                        .setNeutralButton(getResources().getString(R.string.custom_density_dialog_button_got), new DialogInterface.OnClickListener() {
+
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                                if (setLcdDensity(newDensityValue)) {
+                                    mCustomDensity.setSummary(newDensityValue + "");
+                                }
+                            }
+                        })
+                        .setPositiveButton(getResources().getString(R.string.custom_density_dialog_button_reboot), new DialogInterface.OnClickListener() {
+
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                if (setLcdDensity(newDensityValue)) {
+                                    PowerManager pm = (PowerManager) getActivity()
+                                            .getSystemService(Context.POWER_SERVICE);
+                                    pm.reboot("Resetting density");
+                                }
+                            }
+                        })
+                        .setNegativeButton(getResources().getString(R.string.cancel),
+                                new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        dialog.dismiss();
+                                    }
+                                })
+                        .create();
+        }
+        return null;
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mCustomDensity) {
+            String strValue = (String) newValue;
+            if (strValue.equals(getResources().getString(R.string.custom_density))) {
+                showDialog(DIALOG_DENSITY);
+                return true;
+            } else {
+                newDensityValue = Integer.parseInt((String) newValue);
+                showDialog(DIALOG_WARN_DENSITY);
+                return true;
+            }
+        } else if (preference == mStockDensity) {
+            newDensityValue = Integer.parseInt((String) newValue);
+            if (setLcdDensity(newDensityValue)) {
+                mStockDensity.setSummary(getResources().getString(
+                    R.string.stock_density_changed_summary) + newDensityValue);
+            }
+            return true;
+        }
+
+        return false;
+    }
+
+    private boolean setLcdDensity(int newDensity) {
+        DensityHelpers.getMount("rw");
+        if (!CMDProcessor.runSuCommand(
+        "busybox sed -i 's|ro.sf.lcd_density=.*|" + "ro.sf.lcd_density" + "=" + newDensity + "|' " + "/system/build.prop")
+        .success())
+        {
+            showRootDeniedInfoDialog();
+            return false;
+        }
+        DensityHelpers.getMount("ro");
+        return true;
+    }
+
+    private void showRootDeniedInfoDialog() {
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(getActivity());
+        alertDialog.setTitle(R.string.su_dialog_error_title);
+        alertDialog.setMessage(R.string.su_dialog_error_message);
+        alertDialog.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+            }
+        });
+        alertDialog.create().show();
+    }
+}