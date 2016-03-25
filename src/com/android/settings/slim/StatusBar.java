0b3d69c2b0d7dba093259d91f22d0510a97616be
SohamJ <soham4jam@gmail.com>
2016-03-25 03:00:14 -0400

    Trigger SystemUI restart for Task manager

--- src/com/android/settings/slim/StatusBar.java
+++ src/com/android/settings/slim/StatusBar.java
@@ -49,6 +49,7 @@ import com.android.internal.logging.MetricsLogger;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.Utils;
+import com.android.settings.util.Helpers;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settings.search.Indexable;
 
@@ -147,6 +148,8 @@ public class StatusBar extends SettingsPreferenceFragment implements OnPreferenc
             boolean enabled = ((SwitchPreference)preference).isChecked();
             Settings.System.putInt(getActivity().getContentResolver(),
                     Settings.System.ENABLE_TASK_MANAGER, enabled ? 1:0);
+            Helpers.restartSystemUI();
+
         }
         return super.onPreferenceTreeClick(preferenceScreen, preference);
     }
